40a608e13a11d26331d9aefc902b6cf73d447b36
Jecé Xavier <jece@alunos.utfpr.edu.br>
2023-06-12 12:24:48 -0300

    feat(User-Register): create home page

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,30 @@
+import * as S from './styles'
+import { Heading, Text } from '@ignite-ui/react'
+
+import previewImage from '../../assets/app-preview.png'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <S.Container>
+      <S.Hero>
+        <Heading as="h1" size="4xl">
+          Agendamento descomplicado
+        </Heading>
+        <Text size="xl">
+          Conecte seu calendário e permita que as pessoas marquem agendamentos
+          no seu tempo livre.
+        </Text>
+      </S.Hero>
+      <S.Preview>
+        <Image
+          src={previewImage}
+          alt="Calendário simbolizando aplicação em funcionamento"
+          height={400}
+          quality={100}
+          priority
+        />
+      </S.Preview>
+    </S.Container>
+  )
+}
