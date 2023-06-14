bbdaa301865642458743242cedfed0f1574f3fd4
Jecé Xavier <jece@alunos.utfpr.edu.br>
2023-06-14 00:21:41 -0300

    feat: add next-auth and connect oauth with google

--- src/pages/register/connect-calendar/index.page.tsx
+++ src/pages/register/connect-calendar/index.page.tsx
@@ -1,4 +1,5 @@
 import { Button, Heading, MultiStep, Text } from '@ignite-ui/react'
+import { signIn } from 'next-auth/react'
 import { ArrowRight } from 'phosphor-react'
 import { useState } from 'react'
 import * as C from '../components'
@@ -25,7 +26,11 @@ export default function Register() {
 
         <S.ConnectItem>
           <Text>Google Calendar</Text>
-          <Button variant="secondary" size="sm">
+          <Button
+            variant="secondary"
+            size="sm"
+            onClick={() => signIn('google')}
+          >
             Conectar
             <ArrowRight />
           </Button>
