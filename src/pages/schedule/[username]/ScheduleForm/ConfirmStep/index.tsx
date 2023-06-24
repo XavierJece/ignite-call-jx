477f8723803c4a40246c2b244ff06a9eaa4cc666
Jecé Xavier <jece@alunos.utfpr.edu.br>
2023-06-24 12:04:46 -0300

    feat(Schedule): register scheduling

--- src/pages/schedule/[username]/ScheduleForm/ConfirmStep/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/ConfirmStep/index.tsx
@@ -5,6 +5,8 @@ import { useForm } from 'react-hook-form'
 import { z } from 'zod'
 import * as S from './styles'
 import dayjs from 'dayjs'
+import { useRouter } from 'next/router'
+import { api } from '~/lib/axios'
 
 interface ConfirmStepProps {
   schedulingDate: Date
@@ -31,8 +33,20 @@ export function ConfirmStep({
     resolver: zodResolver(confirmFormSchema),
   })
 
-  function handleConfirmScheduling(data: ConfirmFormData) {
-    console.log(data)
+  const router = useRouter()
+  const username = String(router.query.username)
+
+  async function handleConfirmScheduling(data: ConfirmFormData) {
+    const { name, email, notes } = data
+
+    await api.post(`/users/${username}/schedule`, {
+      name,
+      email,
+      notes,
+      date: schedulingDate,
+    })
+
+    onCancelConfirmation()
   }
 
   const describedDate = dayjs(schedulingDate).format('DD [de] MMMM [de] YYYY')
